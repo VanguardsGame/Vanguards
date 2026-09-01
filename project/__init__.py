4beed43eac25db127f823de2fe59d23c70a2e8aa
DapperPixelguy <gabrielrbtaylor@gmail.com>
2026-09-01 19:45:04 +0100

    Basic view counter

--- project/__init__.py
+++ project/__init__.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from flask import Flask
+from flask import Flask, g, request
 from sqlalchemy.testing.pickleable import User
 
 
@@ -25,6 +25,27 @@ def create_app():
     @login_manager.user_loader
     def load_user(user_id):
         return User.query.get(int(user_id))
+
+    def increment_visits():
+        print("Incrementing")
+        with open('project/static/visits.txt', 'w+') as f:
+            visits = f.read()
+            if visits:
+                visits = int(visits)
+            else:
+                visits = 0
+            f.write(f"{visits + 1}")
+    @app.before_request
+    def before_request():
+        g.new_visitor = not(request.cookies.get('site_visited'))
+
+    @app.after_request
+    def after_request(response):
+        if getattr(g, 'new_visitor', None):
+            increment_visits()
+            response.set_cookie('site_visited', "1", max_age= 60 * 60 * 12, httponly=True, samesite="Lax")
+
+        return response
 
 
     from .main import main
