0dbda7371a92d197d662553e51e537d8bef117cd
DapperPixelguy <gabrielrbtaylor@gmail.com>
2026-09-01 19:45:25 +0100

    Get and pass in current visits

--- project/main.py
+++ project/main.py
@@ -4,4 +4,10 @@ main = Blueprint('main', __name__)
 
 @main.route('/')
 def index():
-    return render_template('index.html')
+    with open('project/static/visits.txt', 'r') as f:
+        visits = f.read()
+        if visits:
+            visits = int(visits)
+        else:
+            visits = 0
+    return render_template('index.html', visits=visits)
